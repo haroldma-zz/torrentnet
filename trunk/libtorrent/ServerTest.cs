0637411aacdd75d319039fba5b43316d8aef8aff
m.siniavine <m.siniavine@98c20d88-111a-0410-a45d-4da0ee2beee0>
2007-01-16 20:10:42 +0000

    Added server to accept incoming connections

--- trunk/libtorrent/ServerTest.cs
+++ trunk/libtorrent/ServerTest.cs
@@ -11,14 +11,29 @@ namespace torrent.libtorrent
     [TestFixture]
     public class ServerTest
     {
+        private Server server;
+        private TestIncommingHandler handler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            handler = new TestIncommingHandler();
+            server = new Server(handler);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            server.Close();
+        }
+
         [Test]
         public void MakeConnection()
         {
-            Server server = new Server();
             server.StartListeningAt(1234);
             AsyncClientSocket socket = new AsyncClientSocket("localhost", 1234);
             bool connected = false;
-            socket.ConnectionEstablished += delegate(object sender, EventArgs e)
+            socket.ConnectionEstablished += delegate
                 {
                     connected = true;
                 };
@@ -26,19 +41,72 @@ namespace torrent.libtorrent
             Thread.Sleep(100);
             Assert.IsTrue(connected);
         }
+
+        [Test]
+        public void HandleIncommingConnections()
+        {
+            server.StartListeningAt(1234);
+            for (int i = 0; i < 10; i++)
+            {
+                AsyncClientSocket socket = new AsyncClientSocket("localhost", 1234);
+                socket.Connect();
+            }
+            Thread.Sleep(100);
+            Assert.AreEqual(10, handler.NumberOfConnections);
+        }
+    }
+
+    internal class TestIncommingHandler : IncommingConnectionHandler
+    {
+        public int NumberOfConnections;
+        public void HandleConnection(ClientSocket socket)
+        {
+            NumberOfConnections += 1;
+            socket.Close();
+        }
+    }
+
+    internal interface IncommingConnectionHandler
+    {
+        void HandleConnection(ClientSocket socket);
     }
 
     internal class Server
     {
+        private Socket socket;
+        private IncommingConnectionHandler handler;
+
+        public Server(IncommingConnectionHandler handler)
+        {
+            this.handler = handler;
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
         public void StartListeningAt(int port)
         {
-            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            serverSocket.Bind(new IPEndPoint(IPAddress.Any, port));
-            serverSocket.Listen(10);
-            serverSocket.BeginAccept(delegate(IAsyncResult result)
-                {
-                    Socket clientSocket = serverSocket.EndAccept(result);
-                }, null);
+            socket.Bind(new IPEndPoint(IPAddress.Any, port));
+            socket.Listen(10);
+            socket.BeginAccept(new AsyncCallback(AcceptCallBack), null);
+        }
+        
+        private void AcceptCallBack(IAsyncResult result)
+        {
+            try
+            {
+                Socket clientSocket = socket.EndAccept(result);
+                handler.HandleConnection(new AsyncClientSocket(clientSocket));
+                socket.BeginAccept(new AsyncCallback(AcceptCallBack), null);
+            }
+            catch (ObjectDisposedException e)
+            {
+                // Ignore, this seems to normal
+                // this exception is raised when a socket is accepting connections and the server is shutting down
+            }
+        }
+
+        public void Close()
+        {
+            socket.Close();
         }
     }
 }
